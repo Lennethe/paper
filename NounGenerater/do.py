5a612c84d8eea93d01f89563b6ffe8a69a55a7a2
Lennethe <happyfulldays79@docomo.ne.jp>
2019-12-29 18:12:31 +0900

    Update do.py

--- NounGenerater/do.py
+++ NounGenerater/do.py
@@ -1,29 +1,16 @@
 import csv
 import pprint
+import stanfordnlp
+import function as f
 
-first = True
 dic = {}
-key = 0
-UP_LIMIT = 1
-
-def read_dic(row):
-    key = 0
-    res = {}
-    for x in row:
-        res[x] = key
-        key += 1
-    return res
+key = ["amod", "obl1", "nsubj1"] 
+UP_LIMIT = 10
+nlp = stanfordnlp.Pipeline()
+#
 
 
-with open('7282_1.csv') as f:
-    reader = csv.reader(f)
-    cnt = 0
-    for row in reader:
-        if first:
-            first = False
-            dic = read_dic(row)
-            continue
-        cnt += 1
-        print(row[dic['reviews.text']])
-        if cnt == UP_LIMIT:
-            break
+reviews = f.get_reviews(UP_LIMIT)
+
+for x in range(UP_LIMIT):
+    f.print_dep(reviews[x],key,nlp)
