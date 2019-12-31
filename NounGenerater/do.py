8d0c3bc89263bd152374a39e707122e342241ebf
Lennethe <happyfulldays79@docomo.ne.jp>
2019-12-31 19:16:01 +0900

    add key

--- NounGenerater/do.py
+++ NounGenerater/do.py
@@ -2,15 +2,20 @@ import csv
 import pprint
 import stanfordnlp
 import function as f
+import warnings
+warnings.filterwarnings('ignore')
+# たまには外してエラー文を見よう
 
+#########
 dic = {}
-key = ["amod", "obl1", "nsubj1"] 
-UP_LIMIT = 10
-nlp = stanfordnlp.Pipeline()
-#
+key = ["amod", "conj1", "nmod1", "nsubj1", "obj1", "obl1", "root1"] 
+judge = ["pos"] #sentiが含んでいいやつ None,N,pos,negからなる
+UP_LIMIT = 30
+#nlp = stanfordnlp.Pipeline()
+########
 
 
 reviews = f.get_reviews(UP_LIMIT)
 
 for x in range(UP_LIMIT):
-    f.print_dep(reviews[x],key,nlp)
+    f.print_dep(reviews[x],key,judge)
