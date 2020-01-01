4e2250d4c721c113731226982eb39f67a3bcaaef
Lennethe <happyfulldays79@docomo.ne.jp>
2020-01-01 17:49:54 +0900

    update

--- NounGenerater/do.py
+++ NounGenerater/do.py
@@ -8,14 +8,26 @@ warnings.filterwarnings('ignore')
 
 #########
 dic = {}
-key = ["amod", "conj1", "nmod1", "nsubj1", "obj1", "obl1", "root1"] 
+key = ["amod", "conj", "nmod", "nsubj", "obj", "obl"] 
+key = [["nsubj"], ["obl"], ["obj"],["nmod"], ["conj", "root"], ["amod", "root"], ["amod", "conj"] ]
+not_pair = False
 judge = ["pos"] #sentiが含んでいいやつ None,N,pos,negからなる
 UP_LIMIT = 30
 #nlp = stanfordnlp.Pipeline()
 ########
-
-
+#nlp = stanfordnlp.Pipeline()
+#doc = nlp("Pleasant 10 min walk along the sea front to the Water Bus. restaurants etc. Hotel was comfortable breakfast was good - quite a variety. Room aircon didn't work very well. Take mosquito repelant!")
+#doc.sentences[0].print_dependencies()
+#####
 reviews = f.get_reviews(UP_LIMIT)
-
+posi = 0
+amount = 0
 for x in range(UP_LIMIT):
-    f.print_dep(reviews[x],key,judge)
+    posi += 1
+    print(posi)
+    arr = f.array_review_to_dep(reviews[x], judge, key, not_pair)
+    amount += len(arr)
+    for val in arr:
+        print(val["pos1"]," -> ",val["word1"], " ", val["pos2"]," -> ",val["word2"])
+
+print(amount)
--- NounGenerater/function.py
+++ NounGenerater/function.py
@@ -1,6 +1,10 @@
 import csv
 import stanfordnlp
-
+from SentiWordNet_y.function import SentiWordNetCorpusReader, SentiSynset
+swn_filename = 'SentiWordNet_y/data/SentiWordNet_3.0.0.txt'
+swn = SentiWordNetCorpusReader(swn_filename)
+nlp = stanfordnlp.Pipeline()
+sub = {"i","we","a","the","you","it","us","due","with","of"}
 
 ## ファイル開く時用,private
 def read_dic(row):
@@ -28,9 +32,10 @@ def get_reviews(num):
             if cnt == num:
                 return res
 
-#dependenceをだす
-def print_dep(sentence, key, nlp):
+#ひとつの文章に着き、dependenceをだす。
+def array_review_to_dep(sentence, judge, key, not_pair = True):
     doc = nlp(sentence)
+    res = []
     for x in range(len(doc.sentences)):
         # doc.sentences[x].print_dependencies()
         words = doc.sentences[x].dependencies_string().splitlines()
@@ -40,5 +45,33 @@ def print_dep(sentence, key, nlp):
             sentence[str(i+1)] = [w[1],w[3],w[5]]
         for i in range(len(words)):
             to = sentence[str(i+1)][1]
-            if sentence[str(i+1)][2] in key:
-                print(sentence[str(i+1)][2]," -> ",sentence[to][2], " ", sentence[str(i+1)][0]+" -> "+sentence[to][0])
+            if to not in sentence:
+                continue
+            if swn.print_word_senti(sentence[to][0].lower()) not in judge and swn.print_word_senti(sentence[str(i+1)][0].lower()) not in judge:
+                continue
+            pos1 = sentence[str(i+1)][2]
+            pos2 = sentence[to][2]
+            word1 = sentence[str(i+1)][0]
+            word2 = sentence[to][0]
+            if word1.lower() in sub or word2.lower() in sub:
+                continue
+            if pair_in_key(pos1, pos2, key, not_pair):
+                tmp = {}
+                tmp["pos1"] = pos1
+                tmp["pos2"] = pos2
+                tmp["word1"] = word1
+                tmp["word2"] = word2
+                res.append(tmp)
+    return res
+
+def pair_in_key(pos1, pos2, key, not_pair = True):
+    if not_pair:
+        return pos1 in key
+    for x in key:
+        if len(x) == 1:
+            if pos1 == x[0] or pos2 == x[0]:
+                return True
+        else:
+            if pos1 == x[0] and pos2 == x[1]:
+                return True
+    return False
