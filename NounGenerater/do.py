00cdec781555fd5c1d8d3d95c9b7dcc72fb339f2
Lennethe <happyfulldays79@docomo.ne.jp>
2020-01-02 13:27:43 +0900

    Update do.py

--- NounGenerater/do.py
+++ NounGenerater/do.py
@@ -12,12 +12,13 @@ key = ["amod", "conj", "nmod", "nsubj", "obj", "obl"]
 key = [["nsubj"], ["obl"], ["obj"],["nmod"], ["conj", "root"], ["amod", "root"], ["amod", "conj"] ]
 not_pair = False
 judge = ["pos"] #sentiが含んでいいやつ None,N,pos,negからなる
-UP_LIMIT = 30
+UP_LIMIT = 35000
+cal = f.Calc()
 #nlp = stanfordnlp.Pipeline()
 ########
-#nlp = stanfordnlp.Pipeline()
-#doc = nlp("Pleasant 10 min walk along the sea front to the Water Bus. restaurants etc. Hotel was comfortable breakfast was good - quite a variety. Room aircon didn't work very well. Take mosquito repelant!")
-#doc.sentences[0].print_dependencies()
+nlp = stanfordnlp.Pipeline()
+doc = nlp("価格の割にはいろいろがっかりするホテルである。犬が自由に連れてこれるため常にロビーやエレベーターの中に犬がいる。＝匂いがある。　廊下も部屋もカーペットであるので匂いや汚れが気になる。スタッフはフレンドリーであるがそれ以上のものはない。場所はいいと思う。駅からは遠いが無料のトローリーバスがあるので便利。")
+doc.sentences[0].print_dependencies()
 #####
 reviews = f.get_reviews(UP_LIMIT)
 posi = 0
@@ -25,9 +26,13 @@ amount = 0
 for x in range(UP_LIMIT):
     posi += 1
     print(posi)
+    if reviews[x] == "":
+        continue
     arr = f.array_review_to_dep(reviews[x], judge, key, not_pair)
     amount += len(arr)
-    for val in arr:
-        print(val["pos1"]," -> ",val["word1"], " ", val["pos2"]," -> ",val["word2"])
+    cal.count_noun(arr)
+    #for val in arr:
+    #    print(val["pos1"]," -> ",val["pos2"], " ", val["word1"]," -> ",val["word2"])
 
+cal.print_count_noun()
 print(amount)
